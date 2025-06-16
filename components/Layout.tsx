cd78c11a0155ca9a10f71af8b0c791fd3fa23d7b
yanarui <yanarui0602@icloud.com>
2025-06-16 23:02:43 +0900

    スクロールバーによる横幅の変化解消

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -19,9 +19,9 @@ export default function Layout({ children }: LayoutProps) {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-          transition={{ duration: 0.4, ease: "easeInOut" }}
+          transition={{ duration: 0.6, ease: "easeInOut" }}
           className="flex-1">
-          <main className="p-6">{children}</main>
+          <main>{children}</main>
         </motion.div>
       </AnimatePresence>
       <Footer />
